61c94f538a851b6d0d2ecbf1a3f3e6643c7f8505
Linow <cloos.richard@gmail.com>
2022-01-08 19:06:39 +0100

    Ajout d'un text area pour code

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,13 +1,73 @@
 import React from 'react'
+import styled from 'styled-components'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 const Home = () => {
+  const codeString = `
+  function createStyleObject(classNames, style) {
+    return classNames.reduce((styleObject, className) => {
+      return {...styleObject, ...style[className]};
+    }, {});
+  }
+  
+  function createClassNameString(classNames) {
+    return classNames.join(' ');
+  }
+  
+  // this comment is here to demonstrate an extremely long line length, well beyond what you should probably allow in your own code, though sometimes you'll be highlighting code you can't refactor, which is unfortunate but should be handled gracefully
+  
+  function createChildren(style, useInlineStyles) {
+    let childrenCount = 0;
+    return children => {
+      childrenCount += 1;
+      return children.map((child, i) => createElement({
+        node: child,
+        style,
+        useInlineStyles,
+        key:\`code-segment-\${childrenCount}-\${i}\`
+      }));
+    }
+  }
+  
+  function createElement({ node, style, useInlineStyles, key }) {
+    const { properties, type, tagName, value } = node;
+    if (type === "text") {
+      return value;
+    } else if (tagName) {
+      const TagName = tagName;
+      const childrenCreator = createChildren(style, useInlineStyles);
+      const props = (
+        useInlineStyles
+        ? { style: createStyleObject(properties.className, style) }
+        : { className: createClassNameString(properties.className) }
+      );
+      const children = childrenCreator(node.children);
+      return <TagName key={key} {...props}>{children}</TagName>;
+    }
+  }`
   return (
-    <div>
+    <HomeContainer>
       <p>Home</p>
-    </div>
+      <SyntaxHighlighter
+        language='javascript'
+        style={docco}
+        wrapLongLines={true}
+      >
+        {codeString}
+      </SyntaxHighlighter>
+    </HomeContainer>
   )
 }
 
 Home.propTypes = {}
 
+const HomeContainer = styled.div`
+  max-width: 100%;
+  background-color: ${props => props.theme.secondaryColor};
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  font-size: 0.75rem;
+`
 export default Home
