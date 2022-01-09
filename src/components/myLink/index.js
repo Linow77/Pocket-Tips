f826131aef3a2e7768eb1c7bba1116e5a09252ae
Linow <cloos.richard@gmail.com>
2022-01-09 10:48:53 +0100

    Create component MyLink

---
+++ src/components/myLink/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const MyLink = props => {
+  const { type, to, children } = props
+
+  return SwitchMyLink(type, to, children)
+}
+
+const SwitchMyLink = (type, to, children) => {
+  switch (type) {
+    case 'NavBar':
+      return <NavBarLink to={to}>{children}</NavBarLink>
+
+    default:
+      return <BaseLink to={to}>{children}</BaseLink>
+  }
+}
+
+MyLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired
+}
+
+const BaseLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+`
+
+const NavBarLink = styled(BaseLink)`
+  margin: 1rem;
+  :hover {
+    text-shadow: 1px 0 0 white;
+  }
+`
+
+export default MyLink
